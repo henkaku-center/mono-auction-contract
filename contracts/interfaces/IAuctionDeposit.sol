dff639056fed6f4756fb5e66640157c20aac0238
nfttakerun <nfttakerun@gmail.com>
2023-08-06 21:27:41 +0900

    issue#1

--- contracts/interfaces/IAuctionDeposit.sol
+++ contracts/interfaces/IAuctionDeposit.sol
@@ -2,6 +2,10 @@
 
 pragma solidity 0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./IAuctionDeposit.sol";
+
 interface IAuctionDeposit {
     // The struct of deposit
     /// @param user: The address of the user
@@ -21,7 +25,7 @@ interface IAuctionDeposit {
     event SendToTreasury(address indexed user, uint256 amount);
 
     // Deposit Community Token to the contract
-    function deposit() external payable;
+    function deposit(uint256 amount) external;
 
     // Withdraw Community Token from the contract
     function withdraw(uint256 amount) external;
@@ -41,3 +45,27 @@ interface IAuctionDeposit {
     // すべてのデポジット情報を取得
     function getAllDeposit() external view returns (DepositInfo[] memory);
 }
+
+contract AuctionDeposit is IAuctionDeposit {
+    using SafeERC20 for IERC20;
+
+    IERC20 public token;
+    uint256 constant MAX_DEPOSIT = 2500 * 10**18;
+
+    // This mapping tracks the deposit info of each user
+    mapping(address => uint256) private _deposits;
+
+    constructor(IERC20 _token) {
+        token = _token;
+    }
+
+    function deposit(uint256 amount) external override {
+        uint256 balanceBefore = token.balanceOf(address(this));
+        uint256 balanceAfter = token.balanceOf(address(this));
+        require(balanceAfter - balanceBefore == amount, "Transferred amount does not match requested amount");
+
+        _deposits[msg.sender] += amount;
+        require(_deposits[msg.sender] <= MAX_DEPOSIT, "Deposit limit exceeded");
+
+        emit Deposit(msg.sender, amount);
+    }
