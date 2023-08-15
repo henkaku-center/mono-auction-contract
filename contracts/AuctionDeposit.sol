dfd749c76677162743ca31ec32c683315528d5bc
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-08-15 23:50:29 +0900

    fix test

--- contracts/AuctionDeposit.sol
+++ contracts/AuctionDeposit.sol
@@ -5,40 +5,38 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IAuctionDeposit.sol";
-import "./MockERC20.sol";
 
 contract AuctionDeposit is IAuctionDeposit {
     using SafeERC20 for IERC20;
 
-    IERC20 public token;
-    uint256 constant MAX_DEPOSIT = 2500 * 10 ** 18;
+    address public communityTokenAddr;
+    uint256 public maxDeposit = 2500 * 10 ** 18;
 
     // This mapping tracks the deposit info of each user
     mapping(address => uint256) private _deposits;
 
-    constructor(IERC20 _token) {
-        token = _token;
+    constructor(address _token) {
+        communityTokenAddr = _token;
     }
 
     function deposit(uint256 amount) external override {
-    require(amount > 0, "Amount should be greater than 0");
-    require(_deposits[msg.sender] + amount <= MAX_DEPOSIT, "Deposit limit exceeded");
+        require(amount > 0, "AuctionDeposit: Amount should be greater than 0");
+        require(
+            _deposits[msg.sender] + amount <= maxDeposit,
+            "AuctionDeposit: Deposit limit exceeded"
+        );
 
-    uint256 balanceBefore = token.balanceOf(address(this));
+        bool _success = IERC20(communityTokenAddr).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        require(_success, "AuctionDeposit: Community token transfer failed");
 
-    MockERC20(address(token)).transferFrom(msg.sender, address(this), amount);
+        _deposits[msg.sender] += amount;
 
-    uint256 balanceAfter = token.balanceOf(address(this));
-    require(
-        balanceAfter - balanceBefore == amount,
-        "ERC20 Token transfer failed"
-    );
-
-    _deposits[msg.sender] += amount;
-
-    emit Deposit(msg.sender, amount);
-}
-
+        emit Deposit(msg.sender, amount);
+    }
 
     //仮で入れてるのであとから実装し直す必要あり
     function withdraw(uint256 amount) external override {
@@ -48,7 +46,7 @@ contract AuctionDeposit is IAuctionDeposit {
         );
 
         _deposits[msg.sender] -= amount;
-        token.safeTransfer(msg.sender, amount);
+        IERC20(communityTokenAddr).safeTransfer(msg.sender, amount);
 
         emit Withdraw(msg.sender, amount);
     }
@@ -77,7 +75,7 @@ contract AuctionDeposit is IAuctionDeposit {
 
     //仮で入れてるのであとから実装し直す必要あり
     function sendToTreasury(uint256 amount) external override {
-        token.safeTransfer(msg.sender, amount);
+        IERC20(communityTokenAddr).safeTransfer(msg.sender, amount);
         emit SendToTreasury(msg.sender, amount);
     }
 }
--- test/deposit.ts
+++ test/deposit.ts
@@ -1,44 +1,66 @@
 import { ethers } from 'hardhat'
-import { Contract, Signer } from 'ethers'
-import { expect } from 'chai'
+import { expect, use } from 'chai'
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import { AuctionDeposit, MockERC20 } from '../typechain-types'
+import { formatEther, parseEther } from 'ethers'
 
 describe('AuctionDeposit', function () {
-  let accounts: Signer[]
-  let auction: Contract
-  let token: Contract // This should be a mock ERC20 token for testing
+  let user1: SignerWithAddress
+  let tokenContract: MockERC20 // This should be a mock ERC20 token for testing
+  let auctionDepositContract: AuctionDeposit
 
   beforeEach(async function () {
-    accounts = await ethers.getSigners()
+    ;[user1] = await ethers.getSigners()
 
-    const initialSupply = ethers.utils.parseEther('1000000')
-    const Token = await ethers.getContractFactory('MockERC20')
-    token = await Token.deploy('My Token', 'MTK', initialSupply)
+    const initialSupply = parseEther('1000000')
+    tokenContract = await ethers.deployContract('MockERC20', [
+      'My Token',
+      'MTK',
+      initialSupply,
+    ])
+    await tokenContract.waitForDeployment()
 
-    const tokenAddress: string = token.address
-    console.log('Token Address:', token.address)
+    const tokenAddress: string = await tokenContract.getAddress()
 
-    const Auction = await ethers.getContractFactory('AuctionDeposit')
-    auction = await Auction.deploy(tokenAddress)
-
-    await token.deployed()
-    await auction.deployed()
+    auctionDepositContract = await ethers.deployContract('AuctionDeposit', [
+      tokenAddress,
+    ])
+    await auctionDepositContract.waitForDeployment()
   })
 
   it('should allow users to deposit tokens', async function () {
-    const initialBalance = await token.balanceOf(accounts[0].address)
+    const initialBalance = await tokenContract.balanceOf(user1.address)
 
-    await token.transfer(auction.address, ethers.utils.parseEther('2500'))
-    await auction.deposit(token.address, ethers.utils.parseEther('2500'))
-
-    const finalBalance = await token.balanceOf(accounts[0].address)
-    expect(finalBalance).to.equal(
-      initialBalance.sub(ethers.utils.parseEther('2500'))
+    // user1が持っているトークンをAuctionDepositに払い込むためにtokenコントラクトのapprove関数で承認する
+    await tokenContract.approve(
+      await auctionDepositContract.getAddress(),
+      parseEther('2500')
     )
+    // auctionDepositコントラクトのdeposit関数を呼び出す
+    const runDeposit = await auctionDepositContract.deposit(parseEther('2500'))
+    await runDeposit.wait()
+
+    const finalBalance = await tokenContract.balanceOf(user1.address)
+    expect(
+      Number(formatEther(initialBalance)) - Number(formatEther(finalBalance))
+    ).to.equal(2500)
+
+    // auctionDepositコントラクトに2500ちゃんと移行しているか確認
+    const auctionDepositBalance = await tokenContract.balanceOf(
+      await auctionDepositContract.getAddress()
+    )
+    expect(Number(formatEther(auctionDepositBalance))).to.equal(2500)
+
+    // depositContractに保存されているuser1のdeposit額の確認
+    const user1DepositBalance =
+      await auctionDepositContract.getDepositByAddress(user1.address)
+    expect(user1DepositBalance.user).to.equal(user1.address)
+    expect(Number(formatEther(user1DepositBalance.amount))).to.equal(2500)
   })
 
   it('should not allow users to deposit more than the maximum', async function () {
     await expect(
-      auction.deposit(token.address, ethers.utils.parseEther('3000'))
-    ).to.be.revertedWith('Deposit limit exceeded')
+      auctionDepositContract.deposit(parseEther('3000'))
+    ).to.be.revertedWith('AuctionDeposit: Deposit limit exceeded')
   })
 })
