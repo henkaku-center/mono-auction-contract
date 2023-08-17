c878e0f45e6b9fbed1dc30cb7bd1de8031cc4f0c
Masumi Kawasaki <geeknees@gmail.com>
2023-08-17 21:08:47 +0900

    Refactor hardhat configuration and test scripts
    - Removed unused import of `@nomiclabs/hardhat-ethers` in `hardhat.config.ts`.
    - Removed `@nomiclabs/hardhat-ethers` dependency from `package.json`.
    - Removed trailing spaces and unused imports in `deposit.ts` and `withdraw.ts`.
    - Updated addresses to use `getAddress()` method in `withdraw.ts`.
    - Removed `@nomiclabs/hardhat-ethers` from yarn.lock.
    
    This commit cleans up the code by removing unused dependencies, imports, and unnecessary spaces, improving the overall code quality and maintainability.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import 'hardhat-contract-sizer'
-import '@nomiclabs/hardhat-ethers';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
